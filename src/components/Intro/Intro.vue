08c4b855d962cdee02be0622961d80be4bc35dc8
Lars <larsbreuren@gmail.com>
2021-08-23 11:12:43 +0200

    cleanup & New intro screen

--- src/components/Intro/Intro.vue
+++ src/components/Intro/Intro.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="container">
-    <h1 class="text-white pt-64"> Welkom bij biker buddy! </h1>
-    <router-link class="ctaButton mt-16" to="/Details">Begin</router-link>
+
+  <div class="container relative h-full">
+    <div class="contentcontainer pt-64">
+      <div class="content">
+        <h1 class="text-white"> Welkom bij biker buddy! </h1>
+        <router-link class="ctaButton mt-16" to="/Details">Begin</router-link>
+      </div>
+      <div class="blur"></div>
+    </div>
+    <div class="bg"> </div>
   </div>
 </template>
 
-<style
-  scoped
-  lang="scss"
-  src="@/components/Intro/intro.scss"
-></style>
+<style scoped lang="scss" src="@/components/Intro/intro.scss"></style>
 <script src="@/components/Intro/intro.ts"></script>
