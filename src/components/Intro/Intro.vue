9bc078a362e32179b459edcae240fbb5ef3f97f9
Lars <larsbreuren@gmail.com>
2021-08-23 11:59:42 +0200

    Intro updated

--- src/components/Intro/Intro.vue
+++ src/components/Intro/Intro.vue
@@ -3,14 +3,16 @@
   <div class="container relative h-full">
     <div class="contentcontainer pt-64">
       <div class="content">
-        <h1 class="text-white"> Welkom bij biker buddy! </h1>
-        <router-link class="ctaButton mt-16" to="/Details">Begin</router-link>
+        <h1 class="text-white text-6xl"> Biker buddy </h1>
+        <p class="subtitle text-white "> Your virtual riding buddy </p>
       </div>
       <div class="blur"></div>
     </div>
+    <router-link style="width: 100px;" class="z-10 fixed ctaButton bottom-0 mb-64 left-0 right-0 mx-auto" to="/Details">Begin</router-link>
     <div class="bg"> </div>
   </div>
 </template>
 
 <style scoped lang="scss" src="@/components/Intro/intro.scss"></style>
 <script src="@/components/Intro/intro.ts"></script>
+
