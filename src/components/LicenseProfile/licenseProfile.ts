78efa38a7320afba7d9566a1467836f7fc03a7ac
Lars <larsbreuren@gmail.com>
2021-09-01 11:22:54 +0200

    Animated profile bars

--- src/components/LicenseProfile/licenseProfile.ts
+++ src/components/LicenseProfile/licenseProfile.ts
@@ -3,6 +3,7 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({})
 export default class Headless extends Vue {
     protected checkLicense(license){
+        this.delay();
         if (license == 'A1' || license == 'A2' || license == 'A'){
             return true;
         } else{
@@ -10,10 +11,23 @@ export default class Headless extends Vue {
         }
     }
 
-    protected Onderhoud = this.$store.state.driverScore.Onderhoud;
-    protected Rijvaardigheid = this.$store.state.driverScore.Rijvaardigheid;
-    protected Advies = this.$store.state.driverScore.Advies;
-    protected Coaching = this.$store.state.driverScore.Coaching;
+    protected Onderhoud = 0;
+    protected Rijvaardigheid = 0;
+    protected Advies = 0;
+    protected Coaching = 0;
+
+    protected delay() {
+        setTimeout( () => {
+            this.applyScores();
+        }, 900);
+    }
+
+    protected applyScores(){
+        this.Onderhoud = this.$store.state.driverScore.Onderhoud;
+        this.Rijvaardigheid = this.$store.state.driverScore.Rijvaardigheid;
+        this.Advies = this.$store.state.driverScore.Advies;
+        this.Coaching = this.$store.state.driverScore.Coaching;
+    }
 
 }
 
