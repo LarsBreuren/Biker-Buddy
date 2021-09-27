54baba7e05350bffc1ae76f70b92e1593198a06d
Lars <larsbreuren@gmail.com>
2021-09-27 18:33:27 +0200

    Added new structure to articles

--- src/components/Articles/Article.vue
+++ src/components/Articles/Article.vue
@@ -7,17 +7,16 @@
       :blackWhite="false" />
     <div class="articleContainer">
       <p>{{ article.text1}} </p>
-      <!-- <FullWidthImage style="height: 200px;" class="mb-16" v-bind:image-link="imagelink3"
-        text="Je ketting heeft wat te voorduren!" :overlay="true" :blackWhite="false" />
+      <FullWidthImage style="height: 200px;" class="mb-16" v-bind:image-link="article.img1"
+        :text="article.imgText" :overlay="true" :blackWhite="false" />
 
-      <p>text3</p>
+       <p>{{ article.text2}} </p>
 
-      <FullWidthImage style="height: 200px;" class="mb-16" v-bind:image-link="imagelink2" text="" :overlay="false"
-        :blackWhite="false" /> -->
+      <FullWidthImage style="height: 200px;" class="mb-16" v-bind:image-link="article.img2" text="" :overlay="false"
+        :blackWhite="false" /> 
 
-      <p class="text-m"> Teksten afkomstig van <a class="text-m text-yellow"
-          href="https://www.louis.nl/magazin/rund-ums-bike/schraubertipps/kettenpflege">louis</a> </p>
-
+      <p class="text-m "> Teksten afkomstig van <a class="text-yellow" target="_blank"
+          :href="article.sourceLink">{{source}} {{ article.source }}</a></p>
     </div>
   </div>
 </template>
@@ -26,25 +25,4 @@
 <script src="./article.ts"></script>
 
 
-// <div class="articleContainer">
-//       <p> </p>
-
-//       <p>Zij moet elke dag de enorme koppels aankunnen waarmee de motor aan haar trekt en zij wordt voortdurend
-//         blootgesteld aan vocht en vuil. Zij wrijft zich letterlijk stuk voor ons. Bovendien is de kettingaandrijving nog
-//         steeds de meest eenvoudige manier om een motor aan te drijven.</p>
-//       <FullWidthImage style="height: 200px;" class="mb-16" v-bind:image-link="imagelink3"
-//         text="Je ketting heeft wat te voorduren!" :overlay="true" :blackWhite="false" />
-
-//       <p>De kettingaandrijving is lichter en biedt een effectievere krachtoverbrenging dan de cardanaandrijving, is
-//         ongevoeliger dan de tandriem en dus technisch nog altijd bij de tijd. Daar moeten we dan maar wat onderhoudswerk
-//         voor over hebben. Goed onderhouden gaat een ketting dubbel zo lang mee dan een verwaarloosd exemplaar en zal ook
-//         meer motorvermogen op het achterwiel overbrengen.</p>
-//       <FullWidthImage style="height: 200px;" class="mb-16" v-bind:image-link="imagelink2" text="" :overlay="false"
-//         :blackWhite="false" />
-
-//       <p class="text-m"> Teksten afkomstig van <a class="text-m text-yellow"
-//           href="https://www.louis.nl/magazin/rund-ums-bike/schraubertipps/kettenpflege">louis</a> </p>
-
-//     </div>
-
-// bron gif: http://4.bp.blogspot.com/-rPfumWgjLMg/Uo1xk-uP8uI/AAAAAAAABw4/NbfZXUi19Ig/s1600/aQqXpx8_460sa.gif
+//  Louis bron: https://www.louis.nl/magazin/rund-ums-bike/schraubertipps
